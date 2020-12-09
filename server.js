06406a24d0734dbdc12c68e67c76df6205677195
zelusp <agostiniworks@gmail.com>
2020-12-08 21:15:01 -0600

    add boilerplate server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const uWS = require('uWebSockets.js');
+const { StringDecoder } = require('string_decoder');
+const decoder = new StringDecoder('utf8');
+
+// an "app" is much like Express.js apps with URL routes,
+// here we handle WebSocket traffic on the wildcard "/*" route
+const app = uWS.App().ws('/*', {  // handle messages from client
+
+  open: (socket, req) => {
+    /* For now we only have one canvas */
+    socket.subscribe("drawing/canvas1");
+  },
+  message: (socket, message, isBinary) => {
+    /* In this simplified example we only have drawing commands */
+    app.publish("drawing/canvas1", message, true);
+  }
+});
+
+// finally listen using the app on port 9001
+app.listen('0.0.0.0', Number.parseInt(process.env.PORT) || 9001, (token => {
+    if (token) {
+        console.log(`listening to port ${process.env.PORT || 9001}`);
+    } else {
+        console.log('failed to listen')
+    }
+}))
